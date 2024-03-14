0ae747a178071e558acae8adc75b2984c080c2ea
mjay <michael@merged.group>
2024-03-14 15:30:13 -0400

    added pages

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,49 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      {
+        path: '/sign-up',
+        name: 'Sign Up',
+        component: () => import('pages/SignUp.vue'),
+      },
+      {
+        path: '/login',
+        name: 'Login',
+        component: () => import('pages/Login.vue'),
+      },
+      {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: () => import('pages/Dashboard.vue'),
+      },
+      {
+        path: '/get-started',
+        name: 'Get Started',
+        component: () => import('pages/GetStarted.vue'),
+      },
+      {
+        path: '/verify-email',
+        name: 'Verify Email',
+        component: () => import('pages/VerifyEmail.vue'),
+      },
+      {
+        path: '/add-organization',
+        name: 'Add Organization',
+        component: () => import('pages/CreateOrganization.vue'),
+      },
+      {
+        path: '/integrations',
+        name: 'Integration Home',
+        component: () => import('pages/IntegrationHome.vue'),
+      },
+      {
+        path: '/onboard',
+        name: 'Onboard Assistant',
+        component: () => import('pages/OnboardSetupAssistant.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
