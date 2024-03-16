92eacde3f22242071dfed002bcc0e48c4a5b568f
mjay <michael@merged.group>
2024-03-15 23:39:42 -0400

    removed unused file

--- src/typings.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.svg?component' {
-  import { DefineComponent } from 'vue';
-  const component: DefineComponent<
-    Record<string, unknown>,
-    Record<string, unknown>,
-    unknown
-  >;
-  export default component;
-}
