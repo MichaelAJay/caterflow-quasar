5ccad2bc68086c826e140e9a45d46a4126946924
JBirdy13 <erinef13@gmail.com>
2024-03-23 14:51:54 -0400

    Started on custom button

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <q-btn
+      color="{ isPrimary ? 'black' : 'white' }"
+      flat
+      @click="handleClick"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+let props = defineProps({
+  label: String,
+  isPrimary: Boolean,
+  handleClick: () => {},
+});
+</script>
